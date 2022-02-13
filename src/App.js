17a70972f93f7831ce3472c45e3bf50d19ecb0ef
dextrousracer82 <95047057+dextrousracer82@users.noreply.github.com>
2022-02-13 12:28:32 -0800

    test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+const modal = (title) => {
+  let m = (
+   <div>
+     <h1>{title}</h1>
+   </div>
+  );
+  ReactDOM.render( m, document.getElementById('root'))
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>5</h1>
+      <button onClick={modal}>Press Me!!!</button>
     </div>
   );
 }
