4873ee2f8052b308ebc61aa40b52c7b39d24a899
dextrousracer82 <95047057+dextrousracer82@users.noreply.github.com>
2022-02-14 16:57:10 -0800

    added tailwind

--- src/App.js
+++ src/App.js
@@ -3,21 +3,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-const modal = (title) => {
-  let m = (
-   <div>
-     <h1>{title}</h1>
-   </div>
-  );
-  ReactDOM.render( m, document.getElementById('root'))
-}
 
 function App() {
   return (
-    <div>
-      <h1>5</h1>
-      <button onClick={modal}>Press Me!!!</button>
-    </div>
+    <div></div>
   );
 }
 
