c2bb9fb248f3ca6a88d2c098c88f07bf4dad6a01
dextrousracer82 <95047057+dextrousracer82@users.noreply.github.com>
2022-02-16 17:17:54 -0800

    testingg

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
     <div className='flex bg-slate-700'>
       <Nav />
       <div className='bg-slate-700' id='main'>
-        <h1 className='center'>hi</h1>
+        <h1 className='center'>ih</h1>
       </div>
     </div>
   );
